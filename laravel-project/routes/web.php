3f84ab734131229bd964780cf85f5d1d66940f31
yuta10200831 <yfuse20ki@yahoo.co.jp>
2024-03-24 14:15:07 +0900

    ヘッダーの作成

--- laravel-project/routes/web.php
+++ laravel-project/routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [IncomeController::class, 'index']);
+
+Route::resource('incomes', IncomeController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
